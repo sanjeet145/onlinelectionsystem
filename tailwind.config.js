9c587eb9756379cfeb9ca5e381771a4b490cc168
sanjee145 <sanjeetverma9572@gmail.com>
2024-04-26 22:31:27 +0530

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './src/*.{js, ts, tsx, jsx}',
+    './src/**/*.jsx',
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
 
