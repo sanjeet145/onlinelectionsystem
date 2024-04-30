c5ab87fc1667b06c7b277b964e6d595acc1a1c21
sanjee145 <sanjeetverma9572@gmail.com>
2024-04-30 16:43:59 +0530

    login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,10 +2,10 @@
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import "../pages/css/forms.css";
-import { useRouter } from "next/navigation";
+import {useRouter} from "next/navigation";
 
 export default function Login() {
-    const router = useRouter();
+    const router =useRouter();
     const [form, setForm] = useState({
         voter: "",
         pass: "",
@@ -26,56 +26,56 @@ export default function Login() {
     const togglePasswordVisibility = () => {
         setShowPassword((prev) => !prev);
     };
-
+    
 
     useEffect(() => {
         const link = document.querySelector('.admin');
-
+    
         const handleClick = () => {
-            setisAdmin(!isAdmin);
-        };
-
+        setisAdmin(!isAdmin);
+    };
+    
         if (link) {
-            link.addEventListener('click', handleClick);
+          link.addEventListener('click', handleClick);
         }
-
+    
         return () => {
-            if (link) {
-                link.removeEventListener('click', handleClick);
-            }
+          if (link) {
+            link.removeEventListener('click', handleClick);
+          }
         };
-    }, [isAdmin]);
-
-    const onsubmit = async (e: any) => {
+      }, [isAdmin]); 
+    
+    const onsubmit = async(e:any) => {
         e.preventDefault();
         setloading(!loading);
-        try {
+        try{
             let response;
-            if (isAdmin) {
+            if(isAdmin){
                 response = await fetch("/api/users/admin", {
                     method: 'POST',
                     headers: { 'Content-Type': 'application/json' },
-                    body: JSON.stringify({ form }),
+                    body: JSON.stringify({form}),
                 });
             }
-            else {
+            else{
                 response = await fetch("/api/users/login", {
-                    method: 'POST',
-                    headers: { 'Content-Type': 'application/json' },
-                    body: JSON.stringify({ form }),
-                });
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ form}),
+            });
             }
             const data = await response.json();
             alert(data.message);
-            console.log(data);
-            router.push('pages/Admin');
-
-        } catch (e) {
+                    if(data.success){
+                        router.push('pages/Admin');
+                    }
+        }catch(e){
             console.log(e);
             alert("not submitted");
         }
-        finally {
-            setloading(false);
+        finally{
+        setloading(false);
         }
     };
     return (
@@ -83,7 +83,7 @@ export default function Login() {
             <div className="form-card">
                 <div className="card1">
                     <form className="formc" onSubmit={onsubmit}>
-                        <a className={isAdmin ? "admin admin-clicked" : "admin"}>Admin</a>
+                    <a className={isAdmin?"admin admin-clicked":"admin"}>Admin</a>
                         <div className="uname">
                             <p>Voter Id</p>
                             <input type="text"
@@ -93,20 +93,20 @@ export default function Login() {
                         <div className="pass">
                             <p>Password</p>
                             <div className="passwordInput">
-                                <input type={showPassword ? 'text' : 'password'}
-                                    name="pass"
-                                    onChange={inputEvent}
-                                    value={form.pass} /><br />
-                                <button
-                                    type="button"
-                                    className="togglePassword"
-                                    onClick={togglePasswordVisibility}
-                                >
-                                    {showPassword ? <img src="svgs/closeeye.svg" /> : <img src="svgs/openeye.svg" />}
-                                </button>
-                            </div>
+                            <input type={showPassword ? 'text' : 'password'}
+                                name="pass"
+                                onChange={inputEvent}
+                                value={form.pass} /><br/>
+                            <button
+                                type="button"
+                                className="togglePassword"
+                                onClick={togglePasswordVisibility}
+                            >
+                                {showPassword ?  <img src="svgs/closeeye.svg"/> : <img src="svgs/openeye.svg"/>}
+                            </button>
                         </div>
-                        <button className="Btn" type="submit">{loading ? "loading" : "Login"}</button>
+                            </div>
+                        <button className="Btn" type="submit">{loading ? "loading":"Login"}</button>
                     </form>
                     <p>Already have account <Link href={"/register"} className="link">Register</Link></p>
                 </div>
