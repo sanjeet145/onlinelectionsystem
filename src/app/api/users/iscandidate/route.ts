429ecdcef464defaeee41096410fd162f60d7e2b
sanjee145 <sanjeetverma9572@gmail.com>
2024-05-08 12:47:34 +0530

    iscandidate route issue

--- src/app/api/users/iscandidate/route.ts
+++ src/app/api/users/iscandidate/route.ts
@@ -2,8 +2,11 @@ import { getDataFromCookie } from "@/helpers/getDataFromCookie";
 import { NextResponse, NextRequest } from "next/server";
 
 export async function GET(request:NextRequest) {
-    const decode= await getDataFromCookie(request);
-    return NextResponse.json({
-        isCandidate: decode.isCandidate,
-    })
+    // const decode= await getDataFromCookie(request);
+     return NextResponse.json({
+            isCandidate: true,
+        })
+    // return NextResponse.json({
+    //     isCandidate: decode.isCandidate,
+    // })
 }
