e9d04fbd32f664f4738d331e679c3ace549f7998
sanjee145 <sanjeetverma9572@gmail.com>
2024-05-13 09:56:44 +0530

    cookie

--- src/app/api/users/candadmin/route.ts
+++ src/app/api/users/candadmin/route.ts
@@ -7,49 +7,58 @@ import Admin from "@/models/admin";
 
 import jwt from "jsonwebtoken";
 
-const getDataFromCookie = async (request: NextRequest) => {
-    const cookie = request.cookies.get("session")?.value || '';
-    try {
-        if (!cookie) {
-            throw new Error('Session cookie not found');
-        }
-        
-        const decoded: any = jwt.verify(cookie, process.env.JWT_SECRET!);
-        return decoded;
-    } catch (error) {
-        console.error('Error decoding JWT:', error);
-        throw error; // Rethrow the error to propagate it further if needed
-    }
-}
+// const getDataFromCookie = async (request: NextRequest) => {
+//     const cookie = request.cookies.get("session")?.value || '';
+//     try {
+//         if (!cookie) {
+//             throw new Error('Session cookie not found');
+//         }
+
+//         const decoded: any = jwt.verify(cookie, process.env.JWT_SECRET!);
+//         return decoded;
+//     } catch (error) {
+//         console.error('Error decoding JWT:', error);
+//         throw error; // Rethrow the error to propagate it further if needed
+//     }
+// }
 
 
 export async function GET(req: NextRequest) {
     await dbConnect();
 
+    // try {
+    //    const  data =await getDataFromCookie(req)
+    const cookie = req.cookies.get("session")?.value || '';
     try {
-       const  data =await getDataFromCookie(req)
+        if (!cookie) {
+            throw new Error('Session cookie not found');
+        }
 
-        // const decode = await getDataFromCookie(req);
-        // console.log(decode);
-            // const admin = await Admin.findOne({ _id: decode.id });
-            // const adminid=admin.adminId;
-            // const adminid= "admin";
-            // // console.log(admin);
-            // const candidates = await Candidate.find({ adminId: adminid });
-            // // console.log(candidates);
-            // if (candidates) {
-            //     return NextResponse.json({
-            //         candidates
-            //     })
-            // }
-            return NextResponse.json({
-               data
-            })
-        }
-        catch(error){
-            const sec=process.env.JWT_SECRET;
-            return NextResponse.json({
-                error
-            })
-        }
+        const decoded: any = jwt.verify(cookie, process.env.JWT_SECRET!);
+        return decoded;
     }
+
+    // const decode = await getDataFromCookie(req);
+    // console.log(decode);
+    // const admin = await Admin.findOne({ _id: decode.id });
+    // const adminid=admin.adminId;
+    // const adminid= "admin";
+    // // console.log(admin);
+    // const candidates = await Candidate.find({ adminId: adminid });
+    // // console.log(candidates);
+    // if (candidates) {
+    //     return NextResponse.json({
+    //         candidates
+    //     })
+    // }
+    // return NextResponse.json({
+    //    data
+    // })
+
+    catch (error) {
+        const sec = process.env.JWT_SECRET;
+        return NextResponse.json({
+            error,cookie
+        })
+    }
+}
