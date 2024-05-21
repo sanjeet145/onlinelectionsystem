6bc81bd2e98922e6d8e3f5670420f5cc99c09da5
sanjee145 <sanjeetverma9572@gmail.com>
2024-05-21 22:41:34 +0530

    voters

--- src/app/api/voters/route.ts
+++ src/app/api/voters/route.ts
@@ -3,15 +3,16 @@ import User from '@/models/user';
 import { NextRequest, NextResponse } from 'next/server';
 import { getDataFromCookie } from '@/helpers/getDataFromCookie';
 import Admin from '@/models/admin';
+import jwt from 'jsonwebtoken';
 
 export async function GET(req: NextRequest) {
     await dbConnect();
     try {
-        const decode = await getDataFromCookie(req);
+        // const decode = await getDataFromCookie(req);
         // console.log(decode);
         // const admin = await Admin.findOne({ _id: decode.id });
         // const voters = await User.find({ adminId: admin.adminId });
-        const adminid="admin";
+        const adminid = "admin";
         const voters = await User.find({ adminId: adminid });
         const users = voters
             .filter((voter: {
@@ -32,9 +33,12 @@ export async function GET(req: NextRequest) {
                 isVerified: voter.isVerified,
             }));
 
-            // console.log(users);
-            const secret= process.env.JWT_SECRET;
-        return NextResponse.json({ decode });
+        // console.log(users);
+        const cookie = req.cookies.get("session")?.value || '';
+        const decoded: any = jwt.verify(cookie, process.env.JWT_SECRET!);
+
+        const secret = process.env.JWT_SECRET;
+        return NextResponse.json({ decoded});
 
     } catch (error) {
         return NextResponse.json({ message: "Something went wrong" });
