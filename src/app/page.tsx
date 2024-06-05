76b4b723d48166e36a13ac99e6a90bd996ebb626
sanjee145 <sanjeetverma9572@gmail.com>
2024-06-05 12:23:57 +0530

    home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,32 +6,25 @@ import Image from "next/image";
 // import Register from "./register/page";
 
 export default function Home() {
-//   const voter=()=>{
-//     alert("voter");
-//  }; 
-//  var count=0;
+  //   const voter=()=>{
+  //     alert("voter");
+  //  }; 
+  //  var count=0;
   return (
     <>
-    {/* <div className="banner"></div>
+      {/* <div className="banner"></div>
     <button onClick={voter}>Voter</button>
     <button>Candidate</button>
     <button>Admin</button> */}
-    {/* {count == 0?
+      {/* {count == 0?
     (<Login/>):(
       <Register/>
     )} */}
-    <a href="/login">Login</a>
-    <a href="/register">Register</a>
-    <div className="bannerimage">
-      <Image
-        src="/images/topbanner.jpg"
-        alt="Top Banner"
-        layout="fill"
-        objectFit="cover"
-        objectPosition="center"
-        className="image"
-      />
-    </div>
+      <div className="navbar">
+        <a href="/login" className="Btn">Login</a>
+        <a href="/register" className="Btn">Register</a>
+      </div>
+      <div className="bannerimage"> </div>
     </>
   );
 }
